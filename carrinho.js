be9c8dc3abf6953868b384f62349ddb1750dd7b6
gisotero <giovanna.sotero.santos@gmail.com>
2026-05-07 15:35:42 -0300

    .

--- carrinho.js
+++ carrinho.js
@@ -1,55 +1,119 @@
 let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
 
 let lista = document.getElementById("lista-carrinho");
+
 let total = 0;
 
 function atualizarCarrinho() {
-    lista.innerHTML = "";
-    total = 0;
+if (carrinho.length === 0) {
 
+    lista.innerHTML = `
+
+    <div class="carrinho-vazio">
+
+        <h2>🛒 Você ainda não tem itens no carrinho</h2>
+
+        <a href="produtos.html">
+            <button>Ir às compras</button>
+        </a>
+
+    </div>
+
+    `;
+
+    document.getElementById("total").innerText = "";
+
+    return;
+}
+
+lista.innerHTML = "";
+
+total = 0;
     carrinho.forEach((produto, index) => {
+
         let item = document.createElement("div");
+
         item.classList.add("item-carrinho");
 
-       item.innerHTML = `
-    <img src="${produto.imagem}">
-    
-    <div class="info">
-        <h3>${produto.nome}</h3>
+        item.innerHTML = `
 
-        <p class="unitario">Unitário: R$ ${produto.preco}</p>
-        <p class="total-item">Total: R$ ${(produto.preco * produto.quantidade).toFixed(2)}</p>
+        <input 
+        type="checkbox"
+        class="checkbox"
+        ${produto.selecionado ? "checked" : ""}
+        onchange="toggleSelecionado(${index})"
+        >
 
-        <div class="quantidade">
-            <button onclick="diminuir(${index})">-</button>
-            <span>${produto.quantidade}</span>
-            <button onclick="aumentar(${index})">+</button>
+        <img src="${produto.imagem}">
+
+        <div class="info">
+
+            <h3>${produto.nome}</h3>
+
+            <p class="unitario">
+                Unitário: R$ ${produto.preco}
+            </p>
+
+            <p class="total-item">
+                Total: R$ ${(produto.preco * produto.quantidade).toFixed(2)}
+            </p>
+
+            <div class="quantidade">
+
+                <button onclick="diminuir(${index})">-</button>
+
+                <span>${produto.quantidade}</span>
+
+                <button onclick="aumentar(${index})">+</button>
+
+            </div>
+
         </div>
-    </div>
-`;
         `;
 
         lista.appendChild(item);
 
-        total += produto.preco * produto.quantidade;
+        if (produto.selecionado) {
+
+            total += produto.preco * produto.quantidade;
+
+        }
+
     });
 
-    document.getElementById("total").innerText = "Total: R$ " + total.toFixed(2);
+    document.getElementById("total").innerText =
+    "Total: R$ " + total.toFixed(2);
 
     localStorage.setItem("carrinho", JSON.stringify(carrinho));
 }
 
 function aumentar(index) {
+
     carrinho[index].quantidade++;
+
     atualizarCarrinho();
 }
 
 function diminuir(index) {
+
     if (carrinho[index].quantidade > 1) {
+
         carrinho[index].quantidade--;
+
     } else {
-        carrinho.splice(index, 1); // remove se chegar em 0
+
+        carrinho.splice(index, 1);
+
     }
+
+    atualizarCarrinho();
+}
+
+function toggleSelecionado(index) {
+
+    carrinho[index].selecionado =
+    !carrinho[index].selecionado;
+
     atualizarCarrinho();
 }
 
--- script.js
+++ script.js
@@ -1,20 +1,98 @@
 function adicionarCarrinho(nome, preco, imagem) {
+
     let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
 
     let produtoExistente = carrinho.find(p => p.nome === nome);
 
     if (produtoExistente) {
-        produtoExistente.quantidade += 1;
+
+        produtoExistente.quantidade++;
+
     } else {
+
         carrinho.push({
+
             nome: nome,
+
             preco: preco,
+
             imagem: imagem,
-            quantidade: 1 // 👈 ESSENCIAL
+
+            quantidade: 1,
+
+            selecionado: true
+
         });
+
     }
 
     localStorage.setItem("carrinho", JSON.stringify(carrinho));
 
     alert("Produto adicionado ao carrinho!");
+}
+function filtrarCategoria(categoria) {
+
+    let produtos = document.querySelectorAll(".card");
+
+    produtos.forEach(produto => {
+
+        if (produto.classList.contains(categoria)) {
+
+            produto.style.display = "block";
+
+        } else {
+
+            produto.style.display = "none";
+        }
+
+    });
+}
+
+function mostrarTodos() {
+
+    let produtos = document.querySelectorAll(".card");
+
+    produtos.forEach(produto => {
+
+        produto.style.display = "block";
+
+    });
+}
+
+function pesquisarProdutos() {
+
+    let pesquisa = document
+    .getElementById("barra-pesquisa")
+    .value
+    .toLowerCase()
+    .trim();
+
+    let produtos = document.querySelectorAll(".card");
+
+    produtos.forEach(produto => {
+
+        let nome = produto
+        .querySelector("h3")
+        .innerText
+        .toLowerCase();
+
+        if (nome.startsWith(pesquisa)) {
+
+            produto.style.display = "block";
+
+        } else {
+
+            produto.style.display = "none";
+        }
+
+    });
+
+    if (pesquisa === "") {
+
+        produtos.forEach(produto => {
+
+            produto.style.display = "block";
+
+        });
+    }
 }
